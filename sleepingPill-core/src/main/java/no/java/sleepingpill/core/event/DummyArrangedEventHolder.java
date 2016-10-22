b615312ed1fe8b056f58991747cb0b45b6deb1da
Anders Karlsen <karlsenanders@gmail.com>
2016-10-22 16:10:57 +0200

    Implemented DummyArrangedEvent

--- sleepingPill-core/src/main/java/no/java/sleepingpill/core/event/DummyArrangedEventHolder.java
+++ sleepingPill-core/src/main/java/no/java/sleepingpill/core/event/DummyArrangedEventHolder.java
@@ -1,15 +1,12 @@
 package no.java.sleepingpill.core.event;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 public class DummyArrangedEventHolder implements ArrangedEventHolder {
     @Override
     public List<ArrangedEvent> allArrangedEvents() {
-        return null;
+        return Collections.singletonList(new ArrangedEvent("6c599656fdd846468bbcab66cfffbbc0","JavaZone 2017","javazone2017"));
     }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(UUID.randomUUID().toString());
-    }
 }

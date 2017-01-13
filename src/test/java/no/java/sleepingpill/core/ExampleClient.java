7681597e6bcd39ade6c67bb3bef283648adbd8f0
Anders Karlsen <karlsenanders@gmail.com>
2017-01-13 18:03:22 +0100

    Added som extra debug test code

--- src/test/java/no/java/sleepingpill/core/ExampleClient.java
+++ src/test/java/no/java/sleepingpill/core/ExampleClient.java
@@ -7,12 +7,15 @@ import org.jsonbuddy.JsonNode;
 import org.jsonbuddy.JsonObject;
 import org.jsonbuddy.parse.JsonParser;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.jsonbuddy.JsonFactory.jsonObject;
@@ -49,28 +52,39 @@ public class ExampleClient {
 
     }
 
-    public String addNewSession(String conferenceId) throws Exception {
+    public String addNewSession(String conferenceId,Optional<String> httpAddrToAddTo) throws Exception {
         JsonObject speakerObj = JsonFactory.jsonObject()
                 .put("name","Darth Vader")
                 .put("email","darth@deathstar.com")
                 .put("data",JsonFactory.jsonObject().put("bio",JsonFactory.jsonObject().put("value","My bio").put("privateData",false)));
+        String titleVal = "My title is blåbærsyltetøy " + LocalDateTime.now().toString();
         JsonObject input = jsonObject()
                 .put("data", jsonObject()
-                        .put("title", jsonObject().put("value", "My title is blåbærsyltetøy").put("privateData", false)))
+                        .put("title", jsonObject().put("value", titleVal).put("privateData", false)))
                 .put("speakers",JsonFactory.jsonArray().add(speakerObj));
 
         System.out.println("Posting: " + input);
 
-        URLConnection conn = new URL(SERVER_ADDRESS + "/conference/" + conferenceId + "/session").openConnection();
+        String urlArrr = httpAddrToAddTo.orElse(SERVER_ADDRESS + "/conference/" + conferenceId + "/session");
+        JsonObject parse = sendDataToServer(input, urlArrr, "POST");
+        System.out.println("result from add session:");
+        System.out.println(parse);
+        return parse.requiredString("id");
+    }
+
+    private JsonObject sendDataToServer(JsonObject input, String urlArrr, String methodType) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) new URL(urlArrr).openConnection();
+        conn.setRequestMethod(methodType);
+        conn.setRequestProperty("Accept-Charset", "UTF-8");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("charset", "UTF-8");
         conn.setDoOutput(true);
+
         try (PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(),"utf-8"))) {
             input.toJson(printWriter);
         }
         try (InputStream is = conn.getInputStream()) {
-            JsonObject parse = JsonParser.parseToObject(is);
-            System.out.println("result from add session:");
-            System.out.println(parse);
-            return parse.requiredString("id");
+            return JsonParser.parseToObject(is);
         }
     }
 
@@ -119,8 +133,13 @@ public class ExampleClient {
 
     public static void main(String[] args) throws Exception {
         ExampleClient exampleClient = new ExampleClient();
+        Optional<String> postNewTo = Optional.ofNullable(args != null && args.length > 0 ? args[0] : null);
+        if (postNewTo.isPresent()) {
+            exampleClient.addNewSession(null,postNewTo);
+            return;
+        }
         String conferenceId = exampleClient.addNewConference();
-        String newSessionId = exampleClient.addNewSession(conferenceId);
+        String newSessionId = exampleClient.addNewSession(conferenceId,postNewTo);
         JsonObject session = exampleClient.readSession(newSessionId);
         String speakerid = session.requiredArray("speakers").get(0,JsonObject.class).requiredString("id");
 
